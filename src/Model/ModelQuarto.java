c85da57b7158c270a7220d7d5e3405ed0719328a
jramalho <totoramalho@gmail.com>
2014-11-22 03:25:42 -0200

    Agora as pastas estao certas

---
+++ src/Model/ModelQuarto.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author Jol
+ */
+public class ModelQuarto {
+    private int cod_quarto;
+    private int num_cam;
+    private int num_acomo;
+    private String tipo_quarto;
+    private boolean disponibilidade_quarto;
+    private float preco_quarto;
+    private int andar;
+    private int num_quarto;
+
+    public int getCod_quarto() {
+        return cod_quarto;
+    }
+
+    public void setCod_quarto(int cod_quarto) {
+        this.cod_quarto = cod_quarto;
+    }
+
+    public int getNum_cam() {
+        return num_cam;
+    }
+
+    public void setNum_cam(int num_cam) {
+        this.num_cam = num_cam;
+    }
+
+    public int getNum_acomo() {
+        return num_acomo;
+    }
+
+    public void setNum_acomo(int num_acomo) {
+        this.num_acomo = num_acomo;
+    }
+
+    public String getTipo_quarto() {
+        return tipo_quarto;
+    }
+
+    public void setTipo_quarto(String tipo_quarto) {
+        this.tipo_quarto = tipo_quarto;
+    }
+
+    public boolean isDisponibilidade_quarto() {
+        return disponibilidade_quarto;
+    }
+
+    public void setDisponibilidade_quarto(boolean disponibilidade_quarto) {
+        this.disponibilidade_quarto = disponibilidade_quarto;
+    }
+
+    public float getPreco_quarto() {
+        return preco_quarto;
+    }
+
+    public void setPreco_quarto(float preco_quarto) {
+        this.preco_quarto = preco_quarto;
+    }
+
+    public int getAndar() {
+        return andar;
+    }
+
+    public void setAndar(int andar) {
+        this.andar = andar;
+    }
+
+    public int getNum_quarto() {
+        return num_quarto;
+    }
+
+    public void setNum_quarto(int num_quarto) {
+        this.num_quarto = num_quarto;
+    }
+    
+}
